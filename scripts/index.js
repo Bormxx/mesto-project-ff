23bbeb541b8b23ec2b0e31ad3c1503586e9f7ec7
Bormxx <m2315226@edu.misis.ru>
2024-01-06 17:54:35 +0300

    Попытка номер один

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+const cardsContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
+function createCards() {
+  // @todo: Темплейт карточки
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardContent = initialCards.map(function(item){
+    return item;
+  });
+  // @todo: Функция создания карточки
+  cardContent.forEach(function(item){
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    // @todo: DOM узлы
+    cardElement.querySelector('.card__title').textContent = item.name;
+    cardElement.querySelector('.card__image').src = item.link;
+    // @todo: Функция удаления карточки
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function(evt){
+      let removeCard = evt.target.closest('.places__item');
+      removeCard.remove();
+    });
+    cardsContainer.append(cardElement);
+  });
+}
 // @todo: Вывести карточки на страницу
+createCards();
+
+
+
+
+
+
+
+
+
+
